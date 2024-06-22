419cf13606726e3cf548c4ac4ae129080cf3956b
Dacron <favouradebimpe63@gmail.com>
2024-06-22 16:30:57 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+
+
+function App() {
+console.log(import.meta.env.VITE_FIREBASE_APP_ID);
+  
+  return (
+    <>
+      Spotify Clone
+    </>
+  );
+}
+
+export default App;
